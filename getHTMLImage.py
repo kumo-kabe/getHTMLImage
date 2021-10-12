046a1463fcebefef9eb296c7db651004a7f617ac
Kohei Kusumoto <tnrfomqzop@yahoo.co.jp>
2021-10-12 16:58:06 +0900

    main処理に画像保存処理を記載

--- getHTMLImage.py
+++ getHTMLImage.py
@@ -1,9 +1,23 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import pprint
+import time
+import urllib.error
+import urllib.request
 
 
-def getHTML(url: str):
+def download_file(url, dst_path):
+    try:
+        with urllib.request.urlopen(url) as web_file:
+            data = web_file.read()
+            with open(dst_path, mode='wb') as local_file:
+                local_file.write(data)
+    except urllib.error.URLError as e:
+        print(e)
+
+
+def get_HTML(url: str):
     """引数で指定されたURLのHTML情報を取得(パース)する。
 
     Args:
@@ -15,22 +29,17 @@ def getHTML(url: str):
     return BeautifulSoup(requests.get(url).content, 'lxml')
 
 
-def getLinkObject(soup: BeautifulSoup):
+def get_Link_Object(soup: BeautifulSoup):
     # ページに含まれるリンクを全て取得する
-    links = [url.get('href') for url in soup.find_all('a')]
-    # class が quote の div 要素を全て取得する
-    #quote_elms = soup.find_all('div', {'class': 'quote'})
-    return links
+    return [url.get('href') for url in soup.find_all('a')]
 
 
-def getImageObject(soup: BeautifulSoup):
+def get_Image_Object(soup: BeautifulSoup):
     # ページに含まれる画像リンクを全て取得する
-    # TODO:HTMLの作りによっては、絶対パスと相対パスの画像リンクが混在する
-    images = [url.get('src') for url in soup.find_all('img')]
-    return images
+    return [url.get('src') for url in soup.find_all('img')]
 
 
-def saveFile(imageList):
+def save_File(imageList):
 
     # 各画像へのリンク先情報を保管したリストを引数に指定
     # これらのリンクをrequestsモジュールに渡し、get メソッドで画像を取得する
@@ -49,23 +58,44 @@ def saveFile(imageList):
     # 元々のファイル名で保存
     for url_image in enumerate(imageList):
         r_image = requests.get(url_image)
+
         filename_image = os.path.basename(url_image)
         with open(save_path + filename_image, 'wb') as f:
             f.write(r_image.content)
+            time.sleep(1)
 
 
 def main():
-    url = 'http://quotes.toscrape.com/'
+    # url = 'http://quotes.toscrape.com/'
+    url = 'https://anige.horigiri.net/?p=322864'
 
-    soup = getHTML(url)
+    soup = get_HTML(url)
+    srcs_list = []
+    for url in soup.find_all('img'):
+        tmp = url.get('src')
+        srcs_list.append(tmp)
+    pprint.pprint(srcs_list)
 
-    links = getLinkObject(soup)
-    print(links)
+    for i, image in enumerate(srcs_list):
+        re = requests.get(image)
+        i += 100
+        save_path = './images/'
+        with open(save_path + f'{i}.' + image.split('.')[-1], 'wb') as f:
+            f.write(re.content)
+            time.sleep(1)
 
-    images = getImageObject(soup)
-    print(images)
+    # images_list = get_Image_Object(soup)
+    # pprint.pprint(images_list)  # リストを整形出力
 
-    # saveFile(images)
+    # request.getメソッドは文字列型のURLのみ受け入れる
+
+    # save_File(images_list)
+
+    # for url in images_list:
+    #     print(url)
+    #     save_path = 'images'
+    #     download_file(url, save_path)
+    #     time.sleep(1)
 
 
 if __name__ == '__main__':
